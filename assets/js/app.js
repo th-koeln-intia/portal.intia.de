49dcc23a004e68d83c80b15df1dbfc65f2b99d03
Marc Schmidt <m_schmidt94@web.de>
2021-03-04 19:24:01 +0100

    add button to change font size

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
 //
+
+function changeFontSize() {
+  var default_layout=document.getElementById("default-layout");
+  if(default_layout.style.fontSize=="150%"){
+    document.getElementById("default-layout").style.fontSize="100%";
+  }else{
+    document.getElementById("default-layout").style.fontSize="150%";
+  } 
+}
